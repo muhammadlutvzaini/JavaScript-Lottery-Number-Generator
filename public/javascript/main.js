ebd7045f4fd1057b859eaa1808806f77745ef42d
muhammadlutvzaini <muhammadlutvzaini@gmail.com>
2021-11-20 17:25:08 +0700

    fixed bug

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -14,11 +14,11 @@ function generate(size, lowest, highest) {
       i--;
     }
   }
-/*
-* Math.floor()  mengembalikan nilai bilangan bulat yang paling terdekat dari nilai argument x.
-* Math.random() menghasilnya angka secara acak.
-* push() menambah satu atau lebih elemen ke akhir.
-*/
+  /*
+   * Math.floor() mengembalikan nilai bilangan bulat yang paling terdekat dari nilai argument x.
+   * Math.random() menghasilnya angka secara acak.
+   * push() menambah satu atau lebih elemen ke akhir.
+   */
   var highestNumber = 0;
   for (var m = 0; m < numbers.length; m++) {
     for (var n = m + 1; n < numbers.length; n++) {
@@ -33,7 +33,7 @@ function generate(size, lowest, highest) {
   // console.log(numbers);
   document.getElementById("numbers").innerHTML = numbers.join(" - ");
   /*
-  * document.getElementById() mengambil sebuah value pada inputan di HTML.
-  * join() Menuliskan elemen-elemen array ke dalam satu string dipisahkan oleh karakter yang kita set.
-  */
+   * document.getElementById() Mengambil sebuah value pada inputan di HTML.
+   * join() Menuliskan elemen-elemen array ke dalam satu string dipisahkan oleh karakter yang kita set.
+   */
 }
